efb4701bd3cbc32172baa4e98ef63d7c0489a0ca
Somov Evgeniy <evgeniy.somov@ilogos.biz>
2020-09-04 11:42:13 +0300

    Обновление теста selenide в методе testSearch1

--- selenide-project/src/test/java/tests/GoogleTest.java
+++ selenide-project/src/test/java/tests/GoogleTest.java
@@ -4,17 +4,26 @@ import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.CollectionCondition.*;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.ElementsCollection;
 import org.apache.hc.core5.util.Asserts;
 import org.junit.jupiter.api.Test;
+
 import support.StepsObject.GoogleSteps;
 
 public class GoogleTest {
     @Test
     void testSearch1() {
+        Configuration.startMaximized = true;
         open("https://www.google.com/");
         element(byName("q")).setValue("GeForce 1650").pressEnter();
-        elements("#search .g").shouldHave(size(10));
+        //ElementsCollection list = elements("#search .g").shouldHave(size(9));
+        elements("#search .g").shouldHave(sizeGreaterThanOrEqual(6))
+                .first().shouldHave(text("Игровая видеокарта GeForce GTX 1650 | NVIDIA"))
+                .find(".r>a").click();
+        Wait().until(titleIs("Игровая видеокарта GeForce GTX 1650 | NVIDIA"));
     }
 
     @Test
